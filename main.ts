d5851a0fddd3bd98ab648e139a3339f02e0fe788
Learn by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-07 10:55:21 +0800

    modified on 7 Nov 2024.

--- main.ts
+++ main.ts
@@ -11,6 +11,16 @@ radio.onReceivedNumber(function (receivedNumber) {
         act3 = 1
         xiamiBoard.setIndexColor(0, 0x007fff)
         xiamiBoard.setIndexColor(1, 0x007fff)
+    } else if (receivedNumber == 114) {
+        act4 = 1
+        xiamiBoard.setIndexColor(0, 0xff9da5)
+        xiamiBoard.setIndexColor(1, 0xff9da5)
+    } else if (receivedNumber == 115) {
+        act5 = 1
+        xiamiBoard.setIndexColor(0, 0xff00ff)
+        xiamiBoard.setIndexColor(1, 0xff00ff)
+    } else if (receivedNumber == 123) {
+    	
     } else {
         xiamiBoard.setIndexColor(0, 0xff8000)
         xiamiBoard.setIndexColor(1, 0xff8000)
@@ -25,7 +35,7 @@ function action () {
         xiamiBoard.motorStop(MOTOR.M1)
         xiamiBoard.motorStop(MOTOR.M2)
     }
-    if (act1 == 2) {
+    if (act2 == 1) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 32)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CCW, 32)
         led.plot(1, 0)
@@ -33,13 +43,25 @@ function action () {
         xiamiBoard.motorStop(MOTOR.M1)
         xiamiBoard.motorStop(MOTOR.M2)
     }
-    if (act3 == 3) {
+    if (act3 == 1) {
         xiamiBoard.motorRun(MOTOR.M1, DIRECTION.CW, 32)
         xiamiBoard.motorRun(MOTOR.M2, DIRECTION.CW, 32)
         led.plot(2, 0)
+    } else if (act4 == 1) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Prelude), music.PlaybackMode.InBackground)
+        led.plot(3, 0)
+    } else if (act5 == 1) {
+        blink()
+        led.plot(4, 0)
     } else {
         xiamiBoard.motorStop(MOTOR.M1)
         xiamiBoard.motorStop(MOTOR.M2)
+    }
+}
+function blink () {
+    for (let index = 0; index < 4; index++) {
+        xiamiBoard.setIndexColor(0, xiamiBoard.rgb(152, 0, 0))
+        xiamiBoard.setIndexColor(1, xiamiBoard.rgb(0, 122, 0))
     }
 }
 function init () {
@@ -54,6 +76,8 @@ function init () {
     act2 = 0
     act3 = 0
 }
+let act5 = 0
+let act4 = 0
 let act3 = 0
 let act2 = 0
 let act1 = 0
